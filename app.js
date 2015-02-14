17b00d472234f8ff03e27741d8d3563d50f38daa
lucyq <lucyzqin@gmail.com>
2015-02-14 04:25:56 -0500

    querying data

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
 var routes = require('./routes/index');
 // var users = require('./routes/users');
 var neo4j = require('node-neo4j');
@@ -62,6 +63,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
 
 app.use('/', routes);
 //    
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var db = require("seraph")({ server: process.env['GRAPHENEDB_URL'] || 'http://localhost:7474'})
 var bodyParser = require('body-parser');
 var url = require('url');
-
+var assert = require('assert');
 
 
 
@@ -22,14 +22,48 @@ router.get('/', function(req, res, next) {
 // });
 
 // Data request
-router.get('/data', function(req, res, next) {
+router.get('/data/relationships', function(req, res, next) {
 	var url_parts = url.parse(req.url, true);
 	var node_id = url_parts['query']['node_index'];
 
-    console.log("node id: " + node_id);
-    db.read(node_id, function(err, node) {
-        res.send(node);
+	// ADD A RELATIONSHIP
+	// db.relate(1, 'knows', 2, { for: '2 months' }, function(err, relationship) {
+ //  		assert.deepEqual(relationship, {
+ //    		start: 1,
+	// 	    end: 2,
+	// 	    type: 'knows',
+	// 	    properties: { for: '2 months' },
+	// 	    id: 1
+	// 	});
+	// });
+
+    db.relationships(node_id, 'all', function(err, relationships) {
+    	res.send(relationships);
     });
+
+    // RETURNS INFO ABOUT NODE
+    // db.read(node_id, function(err, node) {
+    // 	db.rel.read()
+    //     res.send(node);
+    // });
+});
+
+router.get('/data/node_info', function(req, res, next) {
+	var url_parts = url.parse(req.url, true);
+	var node_id = url_parts['query']['node_index'];
+
+  	
+    	db.nodesWithLabel('Location', function(err, results) {
+    		console.log("RESULTS: " + results);
+      		res.send([results]); // -> 'DS4'
+    	});
+  	
+	// var data_arr = [];
+// 	for 
+// 	db.read(node_id, function(err, node) {
+// 		data_arr.push(node); 
+// //        res.send(node);
+//     });
 });
 
 
